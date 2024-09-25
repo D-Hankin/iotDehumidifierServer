7f6f7259a30c51db49aa22b82562a48331d53985
David Hankin <d_hankin@outlook.com>
2024-09-25 15:26:24 +0200

    framework and test end points

---
+++ src/main/java/com/iotdehumidifier/iotdehumidifier/IotdehumidifierApplication.java
@@ -0,0 +1,13 @@
+package com.iotdehumidifier.iotdehumidifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotdehumidifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotdehumidifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iotdehumidifier/iotdehumidifier/IotdehumidifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotdehumidifier.iotdehumidifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotdehumidifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
