5fca9d2397afb531174b57302b6f4fb475a607c6
David Hankin <d_hankin@outlook.com>
2024-10-02 20:54:56 +0200

    update, get email dynamically

--- src/main/java/com/iotdehumidifier/iotdehumidifier/services/MailService.java
+++ src/main/java/com/iotdehumidifier/iotdehumidifier/services/MailService.java
@@ -3,6 +3,8 @@ package com.iotdehumidifier.iotdehumidifier.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.iotdehumidifier.iotdehumidifier.models.ArduinoObject;
@@ -14,14 +16,20 @@ public class MailService {
     private JavaMailSender javaMailSender;
 
     public void sendMail(ArduinoObject response) {
-        System.out.println("send email here");
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("d_hankin@outlook.com");
-        message.setSubject("Dehumidifier Service Notification");
-        message.setText("Hi! \n\tThe humidity reading is currently " + response.getHumidity() +"%. It might be a good idea to turn on your dehumidifier!\n\n\tKind regards,\n\n\t\tYour Arduino.");
-        message.setFrom("noreplydehumidifier@gmail.com");
+    
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         
-        javaMailSender.send(message);
+        if (authentication != null && authentication.isAuthenticated()) {
+            String userEmail = authentication.getName(); 
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(userEmail);
+            message.setSubject("Dehumidifier Service Notification");
+            message.setText("Hi! \n\tThe humidity reading is currently " + response.getHumidity() + 
+                                "%. It might be a good idea to turn on your dehumidifier!\n\n\tKind regards,\n\n\t\tYour Arduino.");
+            message.setFrom("noreplydehumidifier@gmail.com");
+            
+            javaMailSender.send(message);
+        }
     }
     
 }
